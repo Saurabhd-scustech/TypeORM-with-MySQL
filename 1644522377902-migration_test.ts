26e7febc06f74764e2bb88dbf44e00a27cff0b9b
Saurabh R. Duryodhan <srduryodhan97@gmail.com>
2022-02-11 12:10:58 +0530

    typeORM completed

--- 1644522377902-migration_test.ts
+++ 1644522377902-migration_test.ts
@@ -9,6 +9,5 @@ export class migrationTest1644522377902 implements MigrationInterface {
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query("ALTER TABLE `user` DROP `bids`")
 
-    }
-
+    
 }
